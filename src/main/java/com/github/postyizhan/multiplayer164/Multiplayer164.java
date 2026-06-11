83b2739649a4767e9784282b20782e5a82843f19
postyizhan <185839426@qq.com>
2026-06-11 14:21:30 +0800

    Bump version to 2.0

--- src/main/java/com/github/postyizhan/multiplayer164/Multiplayer164.java
+++ src/main/java/com/github/postyizhan/multiplayer164/Multiplayer164.java
@@ -15,7 +15,7 @@ import net.minecraftforge.common.MinecraftForge;
 @Mod(modid = Multiplayer164.MODID, version = Multiplayer164.VERSION)
 public class Multiplayer164 {
     public static final String MODID = "Multiplayer164";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "2.0";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
